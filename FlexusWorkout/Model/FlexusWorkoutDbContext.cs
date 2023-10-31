ff7010925d684dfd2d150a2b0206aea19f2fc551
williamdev <william-ca@live.no>
2023-10-31 14:15:42 +0100

    Initial setup of dbcontext and basic setup of user crud operations

--- FlexusWorkout/Model/FlexusWorkoutDbContext.cs
+++ FlexusWorkout/Model/FlexusWorkoutDbContext.cs
@@ -3,15 +3,20 @@ using Microsoft.EntityFrameworkCore;
 
 namespace FlexusWorkout.Model;
 
-public class UserDbContext : DbContext
-{
+public class FlexusWorkoutDbContext : DbContext
+{// One DB context for project to keep things simple
+    public DbSet<User> Users => Set<User>();
+    
+    // set for workouts
+    // set for workout plans
+    
+    
+    
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        optionsBuilder.UseSqlite(@"Data Source = Resources/flexus_workout.db"); // @ means backslashes don't need to be escaped
-        
+        optionsBuilder.UseSqlite(@"Data Source = Resources/School.db"); // @ means backslashes don't need to be escaped
     }
-
-    public DbSet<User> Student => Set<User>();
+    
     
     //protected override void OnModelCreating(ModelBuilder modelBuilder)
     //{
--- FlexusWorkout/Model/UserCrud.cs
+++ FlexusWorkout/Model/UserCrud.cs
@@ -4,8 +4,13 @@ namespace FlexusWorkout.Model;
 
 public class UserCrud
 {
+    private FlexusWorkoutDbContext _flexusWorkoutDbContext = new();
+    //TODO move this and use dependency injection from a wrapper class or something similar? The DBCOntext should be initialized elsewhere
+    //to make sure we don't have connections here
+    
     public User add(User user)
     {
+        _flexusWorkoutDbContext.Users.Add(user);
         return new User();
     }
     
