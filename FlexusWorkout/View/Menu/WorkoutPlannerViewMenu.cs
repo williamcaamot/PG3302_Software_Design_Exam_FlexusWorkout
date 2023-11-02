dd90ec0e0a7d0fe718ff59d5c177d9490705ac16
Markushagen1 <57091267+Markushagen1@users.noreply.github.com>
2023-11-02 13:24:33 +0100

    Adding directory Presenter and WorkutPlannerViewMenu

---
+++ FlexusWorkout/View/Menu/WorkoutPlannerViewMenu.cs
@@ -0,0 +1,8 @@
+namespace FlexusWorkout.View.Menu;
+
+public class WorkoutPlannerViewMenu
+{
+    //Create view that represents the users interface.
+    //SHould be able to select exercices, create weekly workout plans, dsiplay upcoming and existing workouts
+    
+}
