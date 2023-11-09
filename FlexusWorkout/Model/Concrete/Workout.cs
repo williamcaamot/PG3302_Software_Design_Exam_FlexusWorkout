0713ce5730185272bfb4794d87d4f76606dc88cb
williamdev <william-ca@live.no>
2023-11-09 15:29:35 +0100

    New setup of workout class

--- FlexusWorkout/Model/Concrete/Workout.cs
+++ FlexusWorkout/Model/Concrete/Workout.cs
@@ -6,16 +6,16 @@ namespace FlexusWorkout.Model.Concrete;
 public class Workout
 {
     public int? Id { get; set; }
+    public string name { get; set; }
     public string description { get; set; }
-    public string location { get; set; }
     public IList<Exercise> Exercises { get; set; }
-
-
+    public string type { get; set; }
+    public string location { get; set; }
+    
 
     public Workout()
     {
 
-        Exercises = new List<Exercise>();
     }
     
     
