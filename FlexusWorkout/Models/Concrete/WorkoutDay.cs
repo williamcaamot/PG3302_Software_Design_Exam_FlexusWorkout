2132bcc6bfb8c247a230bc3f896536df0c20a0a0
williamdev <william-ca@live.no>
2023-11-14 15:02:13 +0100

    Fix PK WorkoutDay.cs

--- FlexusWorkout/Models/Concrete/WorkoutDay.cs
+++ FlexusWorkout/Models/Concrete/WorkoutDay.cs
@@ -2,7 +2,7 @@ namespace FlexusWorkout.Models.Concrete;
 
 public class WorkoutDay : Base.Model
 { 
-    public int? UserId { get; set; } //PK
+    public int? WorkoutDayId { get; set; } //PK
     public Workout Workout { get; set; }
     public DateOnly date { get; set; }
 
