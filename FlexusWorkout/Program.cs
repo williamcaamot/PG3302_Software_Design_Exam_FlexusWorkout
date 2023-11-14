92e2a71b0d6f26ad22a3e379c1580c1c768f60a1
williamdev <william-ca@live.no>
2023-11-14 21:21:37 +0100

    Edit startup logo

--- FlexusWorkout/Program.cs
+++ FlexusWorkout/Program.cs
@@ -11,24 +11,26 @@ namespace FlexusWorkout
     {
         static void Main(string[] args)
         {
-            
-            Console.WriteLine(@"  ______   _        ______  __   __  _    _    _____                ");
-            Console.WriteLine(@" |  ____| | |      |  ____| \ \ / / | |  | |  / ____|               ");
-            Console.WriteLine(@" | |__    | |      | |__     \ V /  | |  | | | (___                 ");
-            Console.WriteLine(@" |  __|   | |      |  __|     > <   | |  | |  \___ \                ");
-            Console.WriteLine(@" | |      | |____  | |____   / . \  | |__| |  ____) |               ");
-            Console.WriteLine(@" |_|      |______|_|______|_/_/ \_\_ \____/__|_____/    _   _______ ");
-            Console.WriteLine(@" \ \        / /  / __ \  |  __ \  | |/ /  / __ \  | |  | | |__   __|");
-            Console.WriteLine(@"  \ \  /\  / /  | |  | | | |__) | | ' /  | |  | | | |  | |    | |   ");
-            Console.WriteLine(@"   \ \/  \/ /   | |  | | |  _  /  |  <   | |  | | | |  | |    | |   ");
-            Console.WriteLine(@"    \  /\  /    | |__| | | | \ \  | . \  | |__| | | |__| |    | |   ");
-            Console.WriteLine(@"     \/  \/      \____/  |_|  \_\ |_|\_\  \____/   \____/     |_|   ");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine(@"           ______   _        ______  __   __  _    _    _____                ");
+            Console.WriteLine(@"          |  ____| | |      |  ____| \ \ / / | |  | |  / ____|               ");
+            Console.WriteLine(@"          | |__    | |      | |__     \ V /  | |  | | | (___                 ");
+            Console.WriteLine(@"          |  __|   | |      |  __|     > <   | |  | |  \___ \                ");
+            Console.WriteLine(@"          | |      | |____  | |____   / . \  | |__| |  ____) |               ");
+            Console.WriteLine(@"          |_|      |______|_|______|_/_/ \_\_ \____/__|_____/    _   _______ ");
+            Console.WriteLine(@"          \ \        / /  / __ \  |  __ \  | |/ /  / __ \  | |  | | |__   __|");
+            Console.WriteLine(@"           \ \  /\  / /  | |  | | | |__) | | ' /  | |  | | | |  | |    | |   ");
+            Console.WriteLine(@"            \ \/  \/ /   | |  | | |  _  /  |  <   | |  | | | |  | |    | |   ");
+            Console.WriteLine(@"             \  /\  /    | |__| | | | \ \  | . \  | |__| | | |__| |    | |   ");
+            Console.WriteLine(@"              \/  \/      \____/  |_|  \_\ |_|\_\  \____/   \____/     |_|   ");
             Thread.Sleep(2000);
+            
             DatabaseFiller databaseFiller = new();
             databaseFiller.runMySqlScript();
-            //databaseFiller.FillUsers();
-            //databaseFiller.FillExercises();
-            //databaseFiller.fillWorkouts();
+            databaseFiller.FillUsers();
+            databaseFiller.FillExercises();
+            databaseFiller.fillWorkouts();
 
 
 
