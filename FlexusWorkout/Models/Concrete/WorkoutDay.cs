1ca3e9f31f31046ef33b02e7e948e2454cd59e22
williamdev <william-ca@live.no>
2023-11-14 15:01:38 +0100

    add PK to WorkoutDay.cs

--- FlexusWorkout/Models/Concrete/WorkoutDay.cs
+++ FlexusWorkout/Models/Concrete/WorkoutDay.cs
@@ -1,7 +1,13 @@
 namespace FlexusWorkout.Models.Concrete;
 
-public class WorkoutDay
-{
+public class WorkoutDay : Base.Model
+{ 
+    public int? UserId { get; set; } //PK
     public Workout Workout { get; set; }
     public DateOnly date { get; set; }
+
+    public WorkoutDay()
+    {
+    }
+    
 }
