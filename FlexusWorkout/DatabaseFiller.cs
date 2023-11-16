e18855e909e41e36e965deab4671610db040e090
williamdev <william-ca@live.no>
2023-11-16 11:59:19 +0100

    Fix databasefiller to work with new exercise constructors

--- FlexusWorkout/DatabaseFiller.cs
+++ FlexusWorkout/DatabaseFiller.cs
@@ -94,15 +94,15 @@ namespace FlexusWorkout;
             {
                 var balanceExercises = new List<Exercise>
                 {
-                    new BalanceExercise("Balance", "Tree Pose", "A pose that replicates the steady stance of a tree.", 5, null, null, null, 1, "Yoga studio"),
-                    new BalanceExercise("Balance", "Eagle Pose", "A pose that focuses on balance and stretching of the shoulders.", 5, null, null, null, 2, "Yoga studio"),
-                    new BalanceExercise("Balance", "Warrior III", "A challenging pose that combines balance and strength.", 5, null, null, null, 3, "Yoga studio"),
-                    new StrengthExercise("Strength", "Bench Press", "An exercise targeting the chest, shoulders, and triceps.", null, 10, 3, "Bench, Barbell", 4, "Gym"),
-                    new StrengthExercise("Strength", "Deadlift", "A compound movement that works the back, glutes, and legs.", null, 8, 3, "Barbell", 5, "Gym"),
-                    new StrengthExercise("Strength", "Squats", "A lower body exercise focusing on the thighs, hips, and buttocks.", null, 12, 3, "Barbell", 4, "Gym"),
-                    new CardioExercise("Cardio", "Running", "A cardiovascular exercise increasing heart rate and burning calories.", 30, null, null, "Running Shoes", 4, "Outdoor/Track"),
-                    new CardioExercise("Cardio", "Cycling", "An intense cycling workout that improves endurance and strength.", 45, null, null, "Bicycle", 3, "Cycling Studio"),
-                    new CardioExercise("Cardio", "Jump Rope", "Jumping ropes.", 20, null, null, "Jump Rope", 4, "Gym/Outdoor"),
+                    new BalanceExercise("Balance", "Tree Pose", "A pose that replicates the steady stance of a tree.", 5, 1, "Yoga studio"),
+                    new BalanceExercise("Balance", "Eagle Pose", "A pose that focuses on balance and stretching of the shoulders.", 5, 2, "Yoga studio"),
+                    new BalanceExercise("Balance", "Warrior III", "A challenging pose that combines balance and strength.", 5, 3, "Yoga studio"),
+                    new StrengthExercise("Strength", "Bench Press", "An exercise targeting the chest, shoulders, and triceps.", 10, 3, "Bench, Barbell", 4, "Gym"),
+                    new StrengthExercise("Strength", "Deadlift", "A compound movement that works the back, glutes, and legs.", 8, 3, "Barbell", 5, "Gym"),
+                    new StrengthExercise("Strength", "Squats", "A lower body exercise focusing on the thighs, hips, and buttocks.", 12, 3, "Barbell", 4, "Gym"),
+                    new CardioExercise("Cardio", "Running", "A cardiovascular exercise increasing heart rate and burning calories.", 30, "Running Shoes", 4, "Outdoor/Track"),
+                    new CardioExercise("Cardio", "Cycling", "An intense cycling workout that improves endurance and strength.", 45,"Bicycle", 3, "Cycling Studio"),
+                    new CardioExercise("Cardio", "Jump Rope", "Jumping ropes.", 20,"Jump Rope", 4, "Gym/Outdoor"),
                 };
                 foreach (var exercise in balanceExercises)
                 {
