9992ced35a2d62c5f25c0d0f9f51c7389e71799e
Johan Svendsen <johan.svendsen@hotmail.com>
2023-11-16 11:50:53 +0100

    Reworked constructors for concrete exercises.  Added toString() to exercises. Finished ExerciseSelectorPresenter

--- FlexusWorkout/Models/Concrete/BalanceExercise.cs
+++ FlexusWorkout/Models/Concrete/BalanceExercise.cs
@@ -7,7 +7,20 @@ public class BalanceExercise : Exercise {
     public BalanceExercise() {
     }
 
-    public BalanceExercise(string type, string? name, string? description, int? durationInMinutes, int? repetitions, int? sets, string? equipmentRequired, int? intensityLevel, string? location) : 
-        base(type, name, description, durationInMinutes, repetitions, sets, equipmentRequired, intensityLevel, location) {
+    public BalanceExercise(string type, string? name, string? description, int? durationInMinutes, int? intensityLevel, string? location) : 
+        base(type, name, description, durationInMinutes, null, null, null, intensityLevel, location) {
+    }
+    
+
+    public override string ToString()
+    {
+        return $"{Name}" +
+               "\r\n---------------" +
+               $"\r\n{Description}" +
+               "\r\n---------------" +
+               $"\r\nType: {Type}" +
+               $"\r\nIntensity level: {IntensityLevel}" +
+               $"\r\nDuration (minutes): {DurationInMinutes}" +
+               $"\r\nSuggested location: {Location}";
     }
 }
