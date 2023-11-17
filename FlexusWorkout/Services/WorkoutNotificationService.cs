33b46a268321b5a5206f14ca1e6a732d6b414ef0
williamdev <william-ca@live.no>
2023-11-17 09:27:25 +0100

    added service for notification MT

---
+++ FlexusWorkout/Services/WorkoutNotificationService.cs
@@ -0,0 +1,9 @@
+namespace FlexusWorkout.Services;
+
+public class WorkoutNotificationService
+{
+    // get all workoutdays - create a service for this
+    // figure out what dates to notify
+    //create a dbcontext here (NOT THE SAME AS THE REST OF APPLICATION
+    // Put this in a loop and run it once a day? (if program keeps running
+}
